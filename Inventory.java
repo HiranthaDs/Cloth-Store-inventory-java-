da7a7456c1c0651eb43eb7a5016ac74db1edc939
Hiran_Ds <108652843+HiranthaDs@users.noreply.github.com>
2025-01-08 22:49:09 +0530

    Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,13 +1,10 @@
-// File: Inventory.java
 
 public class Inventory {
     public static void main(String[] args) {
-        // Create some example products
         Product product1 = new Product("T-Shirt", "Cotton T-shirt", "M", "Red", 19.99, 50);
         Product product2 = new Product("Jeans", "Denim Jeans", "L", "Blue", 39.99, 30);
         Product product3 = new Product("Jacket", "Winter Jacket", "M", "Black", 59.99, 20);
 
-        // Display product details using the displayProduct() method (if defined in Product class)
         System.out.println("Product 1 Details:");
         product1.displayProduct();
         System.out.println("\nProduct 2 Details:");
@@ -15,7 +12,6 @@ public class Inventory {
         System.out.println("\nProduct 3 Details:");
         product3.displayProduct();
 
-        // Alternatively, you can display using toString()
         System.out.println("\nUsing toString():");
         System.out.println(product1);
         System.out.println(product2);
